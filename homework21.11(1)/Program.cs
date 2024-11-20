52eaaa533dfbf5457956c824dbe480d2ddf6a7c1
huchupuri <squaer120@gmail.com>
2024-11-20 22:44:29 +0300

    Добавьте файлы проекта.

---
+++ homework21.11(1)/Program.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Concurrent;
+
+
+namespace tasks
+{
+    /// <summary>
+    /// перечисление дней недели
+    /// </summary>
+    public enum days { Monday = 1, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday }
+    class Program
+    {
+        static void Task1()
+        {
+            //Дана последовательность из 10 чисел.Определить, является ли эта последовательность
+            //упорядоченной по возрастанию.В случае отрицательного ответа определить
+            //порядковый номер первого числа, которое нарушает данную последовательность.
+            //Использовать break.
+
+            Console.WriteLine("задание 1");
+            Console.Write("введите 10 чисел: ");
+            bool flag = true;
+            int[] nums = new int[10];
+            while (flag)
+            {
+                try
+                {
+                    nums = Console.ReadLine().Split(' ').Select(i => int.Parse(i)).ToArray<int>();
+                }
+                catch (System.FormatException)
+                {
+                    Console.WriteLine("допускается ввод только чисел");
+                    continue;
+                }
+                if (nums.Length != 10) Console.WriteLine("вы ввели не 10 чисел");
+                else flag = false;   
+            }
+
+            bool flag2 = true;
+            for (int i = 0; i < 9; i++)
+            {
+                if (nums[i] > nums[i + 1])
+                {
+                    Console.WriteLine($"последовательносит нарушается на { i + 2} элементе");
+                    flag2 = false;
+                    break;
+                }
+            }
+            if (flag2) Console.WriteLine("последовательность упорядоченна");
+        }
+        static void Task2()
+        {
+            //Игральным картам условно присвоены следующие порядковые номера в зависимости от
+            //их достоинства: «валету» — 11, «даме» — 12, «королю» — 13, «тузу» — 14.
+            //Порядковые номера остальных карт соответствуют их названиям(«шестерка»,
+            //«девятка» и т.п.).По заданному номеру карты k(6 <= k <= 14) определить достоинство
+            //соответствующей карты. Использовать try-catch-finally.
+            Console.WriteLine("задание 2");
+
+            Console.Write("введите номер карты: ");
+            bool cardFlag = true;
+            sbyte? card = null;
+            bool formatCheck = true;
+
+            while (cardFlag)
+            {
+                try
+                {
+                    card = sbyte.Parse(Console.ReadLine());
+
+                }
+                catch (System.FormatException){}
+                finally
+                {
+                    if (card < 14 && card > 6) cardFlag = false;
+                    else Console.WriteLine("номер карты больше 5 и меньше 15");
+                }
+            }
+            Console.Write("карта: ");
+            switch (card)
+            {
+                case 11: Console.WriteLine("валет"); break;
+                case 12: Console.WriteLine("дама"); break;
+                case 13: Console.WriteLine("король"); break;
+                case 14: Console.WriteLine("туз"); break;
+                default: Console.WriteLine(card); break;
+
+            }
+
+        }
+
+        static void Task3()
+        {
+
+            //Напишите программу, которая принимает на входе строку и производит выходные
+            //данные в соответствии со следующей таблицей:
+            Console.WriteLine("задание 3");
+            string input = Console.ReadLine().ToLower();
+            switch (input)
+            {
+                case "jabroni": Console.WriteLine("Patron Tequila"); break;
+                case "school counselor": Console.WriteLine("Anything with alco"); break;
+                case "programmer": Console.WriteLine("hipster craft beer"); break;
+                case "bike gang member": Console.WriteLine("moonshine"); break;
+                case "politician": Console.WriteLine("your tax dollars"); break;
+                case "rapper": Console.WriteLine("blue cristal"); break;
+                default: Console.WriteLine("beer"); break;
+            }
+        }
+
+        static void Task4()
+        {
+            //Составить программу, которая в зависимости от порядкового номера дня недели(1, 2,
+            //..., 7) выводит на экран его название(понедельник, вторник, ..., воскресенье).
+            //Использовать enum.
+
+            Console.WriteLine("задание 4");
+            Console.Write("введите номер дня недели(1-7) ");
+            sbyte day;
+            while ((sbyte.TryParse(Console.ReadLine(), out day) == false) || (day > 7) || (day == 0))
+            {
+                Console.WriteLine("вы ввели неправильное число");
+            }
+            Console.WriteLine((days)day);
+
+        }
+
+        static void Task5()
+        {
+            //Создать массив строк.При помощи foreach обойти весь массив.При встрече элемента
+            //"Hello Kitty" или "Barbie doll" необходимо положить их в “сумку”, т.е.прибавить к
+            //результату.Вывести на экран сколько кукол в “сумке”.
+
+            Console.WriteLine("зажание 5");
+            Console.Write("введите несколько предложений через запятую: ");
+            string[] purchases = Console.ReadLine().Split(',');
+            int bag = 0;
+            foreach (string purchase in purchases)
+            {
+                if (purchase.ToLower() == "barbie doll" || purchase.ToLower() == "hello kitty") bag++;
+            }
+            Console.WriteLine($"В сумке {bag} кукол");
+        }
+        static void Main(string[] args)
+        {
+            Task1();
+            Task2();
+            Task3();
+            Task4();
+            Task5();
+        }
+    }
+}
---
+++ lab1/Program.cs
@@ -0,0 +1,154 @@
+﻿using System;
+
+class Program
+{
+    //        Упражнение 4.1 Написать программу, которая читает с экрана число от 1 до 365(номер дня
+    //в году), переводит этот число в месяц и день месяца. Например, число 40 соответствует 9
+    //февраля(високосный год не учитывать).
+    static void Task1() 
+    {
+        Console.WriteLine("упражнение 4.1");
+        Console.Write("Введите номер дня в году (от 1 до 365): ");
+        int dayOfYear = int.Parse(Console.ReadLine());
+        DateSerching(dayOfYear);
+    }
+
+//    Упражнение 4.2 Добавить к задаче из предыдущего упражнения проверку числа введенного
+//пользователем.Если число меньше 1 или больше 365, программа должна вырабатывать
+//исключение, и выдавать на экран сообщение.
+    static void Task2()
+    {
+        Console.WriteLine("упражнение 4.2");
+        int dayOfYear;
+        do
+        {
+            Console.Write("Введите номер дня в году (от 1 до 365): ");
+
+        }
+        while (!int.TryParse(Console.ReadLine(), out dayOfYear) || dayOfYear >= 365 || dayOfYear <= 1);
+
+        DateSerching(dayOfYear);
+    }
+
+//    Домашнее задание 4.1 Изменить программу из упражнений 4.1 и 4.2 так, чтобы она
+//учитывала год(високосный или нет). Год вводится с экрана. (Год високосный, если он
+//делится на четыре без остатка, но если он делится на 100 без остатка, это не високосный
+//год. Однако, если он делится без остатка на 400, это високосный год.)
+    static void Task3()
+    {
+        Console.WriteLine("дз 4.1");
+        int dayOfLeapYear, year;
+        Console.Write("Введите год: ");
+        while (!int.TryParse(Console.ReadLine(), out year))
+        {
+            Console.WriteLine("введите число");
+        }
+
+        Console.Write("Введите день: ");
+        while (!int.TryParse(Console.ReadLine(), out dayOfLeapYear) || dayOfLeapYear >= 365 || dayOfLeapYear <= 1)
+        {
+            Console.WriteLine("введите число от 1 до 366");
+        }
+        DateSerching(dayOfLeapYear, year);
+    }
+
+    /// <summary>
+    /// поиск числа и месяца
+    /// </summary>
+    static void DateSerching(int dayOfYear)
+    {
+        int[] daysInMonth = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
+
+        int month = 0;
+        int dayOfMonth = dayOfYear;
+
+        for (int i = 0; i < daysInMonth.Length; i++)
+        {
+            if (dayOfMonth > daysInMonth[i])
+            {
+                dayOfMonth -= daysInMonth[i];
+            }
+            else
+            {
+                month = i + 1;
+                break;
+            }
+        }
+
+        Console.WriteLine($"{dayOfMonth} {GetMonthName(month)}.");
+    }
+
+    /// <summary>
+    /// метод по поиску числа и месяца в високосном году
+    /// </summary>
+    static void DateSerching(int dayOfYear, int year)
+    {
+        sbyte[] daysInMonth = new sbyte[12];
+
+        if ((year % 4 == 0) && ((year % 100 != 0) || (year % 400 == 0)))
+        {
+            daysInMonth = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+        }
+        else daysInMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+        int month = 0;
+        int dayOfMonth = dayOfYear;
+
+        for (int i = 0; i < daysInMonth.Length; i++)
+        {
+            if (dayOfMonth > daysInMonth[i])
+            {
+                dayOfMonth -= daysInMonth[i];
+            }
+            else
+            {
+                month = i + 1;
+                break;
+            }
+        }
+
+        Console.WriteLine($"{dayOfMonth} {GetMonthName(month)}.");
+    }
+
+    /// <summary>
+    /// метод выдает месяц соотвествующему числу
+    /// </summary>
+    static string GetMonthName(int month)
+    {
+        switch (month)
+        {
+            case 1: return "января";
+            case 2: return "февраля";
+            case 3: return "марта";
+            case 4: return "апреля";
+            case 5: return "мая";
+            case 6: return "июня";
+            case 7: return "июля";
+            case 8: return "августа";
+            case 9: return "сентября";
+            case 10: return "октября";
+            case 11: return "ноября";
+            case 12: return "декабря";
+            default: return "";
+        }
+    }
+    static void Main(string[] args)
+    {
+//        Упражнение 4.1 Написать программу, которая читает с экрана число от 1 до 365(номер дня
+//в году), переводит этот число в месяц и день месяца. Например, число 40 соответствует 9
+//февраля(високосный год не учитывать).
+
+        Task1();
+
+//        Упражнение 4.2 Добавить к задаче из предыдущего упражнения проверку числа введенного
+//пользователем.Если число меньше 1 или больше 365, программа должна вырабатывать
+//исключение, и выдавать на экран сообщение.
+        Task2();
+
+//        Домашнее задание 4.1 Изменить программу из упражнений 4.1 и 4.2 так, чтобы она
+//учитывала год(високосный или нет). Год вводится с экрана. (Год високосный, если он
+//делится на четыре без остатка, но если он делится на 100 без остатка, это не високосный
+//год.Однако, если он делится без остатка на 400, это високосный год.)
+        Task3();
+    }
+}
